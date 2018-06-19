212e3e0b855f178252d2430fe0a41b43a5a1993a
Gary Mansell <gary.mansell@cyberlytic.com>
2018-06-19 15:36:32 +0100

    Dump due to library bug.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+extern crate rocket_codegen;
+
+use rocket::{Request, State};
+use rocket_contrib::JSON;
+
+struct Person {
+    id: usize,
+    name: String,
+    age: u8,
+    email: String,
+    address: String,
+}
+
+#[get("/<id>")]
+fn get_person(id: usize) -> String {
+    format!("Id {}!", id)
+}
+
+#[post("/<id>")]
+fn post_person(id: usize) -> String {
+    format!("Id {}!", id)
+}
+
+#[delete("/<id>")]
+fn delete_person(id: usize) -> String {
+    format!("Id {}!", id)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let client = Client::connect("localhost", 27017).expect("Failed to connect to db!");
+
+    let people_coll = client.db("flexera").collection("people");
+
+    people_coll
+        .insert_one(
+            doc! {
+                "id": 1,
+                "name": "Bob",
+            },
+            None,
+        )
+        .ok()
+        .expect("Failed to insert document.");
+
+    rocket::ignite()
+        .mount("/people", routes![get_person, post_person, delete_person])
+        .launch();
 }
