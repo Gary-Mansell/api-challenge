ce6dc75da988ae92f7f92e314d47f970e6d909ec
Gary Mansell <gary.mansell@cyberlytic.com>
2018-07-02 20:51:55 +0100

    Some cleanup and partial implementation of find.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(plugin, custom_derive)]
 #![plugin(rocket_codegen)]
 
+#[macro_use]
 extern crate bson;
 extern crate mongodb;
 extern crate rocket;
@@ -10,6 +11,7 @@ extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
+use bson::oid::ObjectId;
 use bson::Bson;
 use mongodb::db::ThreadedDatabase;
 use mongodb::{Client, ThreadedClient};
@@ -18,17 +20,57 @@ use rocket::{Request, State};
 use rocket_contrib::Json;
 use std::sync::{Arc, Mutex};
 
+use std::error::Error;
+use std::fmt;
+
 const PORT: u16 = 8081;
 const DB_HOST: &'static str = "localhost";
 const DB_PORT: u16 = 27017;
 const DB_NAME: &'static str = "MyApp";
 
+#[derive(Debug)]
+struct ApiError {
+    details: String,
+}
+
+impl ApiError {
+    fn new(msg: &str) -> ApiError {
+        ApiError {
+            details: msg.to_string(),
+        }
+    }
+}
+
+impl Error for ApiError {
+    fn description(&self) -> &str {
+        &self.details
+    }
+}
+
+impl fmt::Display for ApiError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.details)
+    }
+}
+
+impl From<mongodb::Error> for ApiError {
+    fn from(err: mongodb::Error) -> Self {
+        ApiError::new(err.description())
+    }
+}
+
+impl From<bson::oid::Error> for ApiError {
+    fn from(err: bson::oid::Error) -> Self {
+        ApiError::new(err.description())
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 struct Person {
-    // #[serde(rename = "_id")]
-    // pub id: String,
+    #[serde(skip_deserializing)] // Do NOT rename = "_id" (conflicts with returned value)
+    id: String,
     name: String,
-    age: i8, // Mongo driver doesn't support unsigned types - (can't use u8)
+    age: i8, // Can't use u8 - Mongo driver doesn't support unsigned types
     email: String,
     address: String,
     balance: f64,
@@ -41,36 +83,74 @@ fn default() -> &'static str {
 }
 
 #[get("/<id>")]
-fn get_person(id: usize) -> &'static str {
+fn get_person(
+    db_client: State<Arc<Mutex<mongodb::Client>>>,
+    id: String,
+) -> Result<String, ApiError> {
+    let db_client = db_client.inner().lock().unwrap();
+    let people_coll = db_client.db(DB_NAME).collection("people");
+    let id: ObjectId = ObjectId::with_string(&id)?;
     println!("Retrieving: {}...", id);
-    "Success!"
+
+    people_coll
+        .count(
+            Some(doc!{
+            "_id": id
+            }),
+            None,
+        )
+        .map_err(|err| ApiError::new(err.description()))
+        .and_then(|count| {
+            println!("Found {} records...", count);
+            Ok(format!("{}", count))
+        })
+
+    // .expect("Failed to execute find!")
+    // match item {
+    //     Some(Ok(doc)) => println!("Found something..."),
+    //     // Some(Err(_)) => panic!("Failed to get next from server!"),
+    //     None => panic!("Server returned no results!"),
+    // }
 }
 
 #[post("/", format = "application/json", data = "<person>")]
-fn post_person(db_client: State<Arc<Mutex<mongodb::Client>>>, person: Json<Person>) -> String {
+fn post_person(
+    db_client: State<Arc<Mutex<mongodb::Client>>>,
+    person: Json<Person>,
+) -> Result<String, ApiError> {
     let db_client = db_client.inner().lock().unwrap();
     let people_coll = db_client.db(DB_NAME).collection("people");
     let person = person.into_inner();
 
     println!("Creating: {:?}", &person);
-    let serialized_person = bson::to_bson(&person).unwrap();
-    if let Bson::Document(document) = serialized_person {
-        people_coll
-            .insert_one(document, None)
-            .ok()
-            .expect("Failed to insert document!");
-    } else {
-        println!("Failed to convert BSON into MongoDB document!");
-    };
-
-    format!("Created {}!", &person.name);
-    format!("Success!")
+    bson::to_bson(&person)
+        .map_err(|err| ApiError::new(err.description()))
+        .and_then(|serialized_person: Bson| {
+            if let Bson::Document(document) = serialized_person {
+                people_coll
+                    .insert_one(document, None)
+                    .map_err(|err| ApiError::new(err.description()))
+                    .and_then(|res| {
+                        let id = res
+                            .inserted_id
+                            .expect("No inserted_id returned!")
+                            .as_object_id()
+                            .map_or(String::new(), |r| r.to_hex());
+                        Ok(id)
+                    })
+            } else {
+                Err(ApiError::new(
+                    "Failed to convert BSON into MongoDB document!",
+                ))
+            }
+        })
 }
 
 #[delete("/<id>")]
-fn delete_person(id: usize) -> String {
+fn delete_person(id: String) -> Result<&'static str, ApiError> {
+    let id: ObjectId = ObjectId::with_string(&id)?;
     println!("Deleting: {}...", id);
-    format!("Deleted {}!", id)
+    Ok("Success!")
 }
 
 #[error(404)]
